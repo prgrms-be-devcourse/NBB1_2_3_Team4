d2512f81567d51195603d7e2f8786f9aca7758de
wndgh <wndgh6@naver.com>
2024-09-23 15:46:05 +0900

    fix gitignore

---
+++ src/main/java/com/example/Nadeuri/NadeuriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nadeuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadeuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadeuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Nadeuri/NadeuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nadeuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadeuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
