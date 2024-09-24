0f81d57f1a941cee5b0e8108483c9ef6f11bac56
gt-park7356 <tlals0028@gmail.com>
2024-09-24 16:34:19 +0900

    jwt 구현 중

--- src/main/java/com/example/Nadeuri/member/security/util/JWTUtil.java
+++ src/main/java/com/example/Nadeuri/member/security/util/JWTUtil.java
@@ -1,24 +1,36 @@
 package com.example.Nadeuri.member.security.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import com.example.Nadeuri.member.dto.JwtTokenDTO;
+import io.jsonwebtoken.*;
+
 import io.jsonwebtoken.security.Keys;
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import org.apache.catalina.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.io.UnsupportedEncodingException;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @Log4j2
 public class JWTUtil {
     private static String key//서명에 사용할 키 - 30자 이상으로
-            = "1234567890123456789012345678901234567890";
-    //JWT 문자열 생성                              //저장 문자열, 만료 시간 - 분 단위
-    public String createToken(Map<String, Object> valueMap, int min) {
+            = "1234567890123456789012345678901234567890123";
+
+    // Member 정보를 가지고 AccessToken, RefreshToken을 생성하는 메서드
+    public JwtTokenDTO createToken(Authentication authentication ) {
         SecretKey key = null;
         try {
             key = Keys.hmacShaKeyFor(JWTUtil.key.getBytes("UTF-8"));
@@ -26,20 +38,60 @@ public class JWTUtil {
             throw new RuntimeException(e);
         }
 
-        Date now = new Date();   //토큰 발행 시간
-        return Jwts.builder().header().add("alg", "HS256")
-                .add("type", "JWT")
-                .and()
-                .issuedAt(now)       //토큰 발행 시간
-                .expiration(         //토큰 만료 시간
-                        new Date( now.getTime() + Duration.ofMinutes(min).toMillis()) )
-                .claims(valueMap)   //저장 데이터
-                .signWith(key)      //서명
+        // 인증된 사용자의 권한 목록을 쉼표로 구분된 문자열로 변환(권한 가져오기)
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+
+
+        //Access 토큰 생성
+        Date accessTokenExpiresIn = new Date(now + 86400000);
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key)
                 .compact();
+
+        //Refresh 토큰 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + 86400000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return JwtTokenDTO.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 
+    //Jwt 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+    public Authentication getAuthentication(String Token) {
+        //Jwt 토큰 복호화
+        Claims claims = parseClaims(Token);
+
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication return
+        // UserDetails: interface, User: UserDetails를 구현한 class
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+
+
     //검증 기능 수행
-    public Map<String, Object> validateToken(String token) {
+    public boolean validateToken(String token) {
         SecretKey key = null;
         try {
             key = Keys.hmacShaKeyFor(JWTUtil.key.getBytes("UTF-8"));
@@ -47,10 +99,35 @@ public class JWTUtil {
             throw new RuntimeException(e);
         }
 
-        Claims claims = Jwts.parser().verifyWith(key).build()
-                .parseSignedClaims(token).getPayload();
-        log.info("--- claim " + claims);
+        try {
+            Jwts.parser()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
 
-        return claims;
+
+    // 주어진 액세스 토큰을 파싱하여 클레임을 반환하고, 만료된 경우 클레임을 반환하는 메서드
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
     }
 }
