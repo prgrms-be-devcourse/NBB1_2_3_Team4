a4f428004ed5a06c1f29abe6f2ab6a79859d6e1b
gt-park7356 <tlals0028@gmail.com>
2024-09-24 16:22:39 +0900

    멤버 엔티티

--- src/main/java/com/example/Nadeuri/member/domain/Member.java
+++ src/main/java/com/example/Nadeuri/member/domain/Member.java
@@ -15,9 +15,35 @@ public class Member {
     @Id
     @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String userId;
-    String email;
-    String password;
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+
+    private String birthDate;
+    private String name;
+    private String profileImage;
+    private String nickname;
+
+
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public void changeRole(String role) {
+        this.role = role;
+    }
+
+    public void changeProfileImage(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
 }
