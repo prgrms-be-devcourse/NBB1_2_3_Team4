a8e18e52993ab825c0a65f3dff82ce597eaf47db
천영상 <chuns7740@gmail.com>
2024-09-24 17:04:14 +0900

    feat : CommentRepository 구현

--- src/main/java/com/example/Nadeuri/comment/CommentRepository.java
+++ src/main/java/com/example/Nadeuri/comment/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.example.Nadeuri.comment;
 
-public interface Repository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByBoardId(Long boardId);        // 게시글에 달려있는 모든댓글 조회
+
+    List<CommentEntity> findByMemberId(Long memberId);      // 유저가 작성한 모든댓글 조회
 }
