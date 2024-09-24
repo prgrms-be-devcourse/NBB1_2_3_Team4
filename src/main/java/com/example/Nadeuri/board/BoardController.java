cc8d712fea7b6c452c414f4000057e7e1ee50fdf
wndgh <wndgh6@naver.com>
2024-09-24 16:13:54 +0900

    feat: 게시글 전체 조회, 검색 조회(제목) 추가

--- src/main/java/com/example/Nadeuri/board/BoardController.java
+++ src/main/java/com/example/Nadeuri/board/BoardController.java
@@ -2,13 +2,17 @@ package com.example.Nadeuri.board;
 
 import com.example.Nadeuri.board.dto.BoardDTO;
 import com.example.Nadeuri.board.dto.request.BoardCreateRequest;
+import com.example.Nadeuri.board.dto.request.BoardPageRequestDTO;
 import com.example.Nadeuri.board.exception.BoardException;
 import com.example.Nadeuri.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,9 +36,24 @@ public class BoardController {
     }
 
     //게시글 상세 조회 (1개 조회)
-    @GetMapping("{id}")
-    public ResponseEntity<ApiResponse> Read(@PathVariable("id") Long boardId){
-        log.info("readService() ---");
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> read(@PathVariable("id") Long boardId){
+        log.info("readController() ---");
         return ResponseEntity.ok(ApiResponse.success(boardService.read(boardId)));
+    }
+
+    //게시글 전체 조회 (1개 조회) --
+    @GetMapping
+    public ResponseEntity<ApiResponse> page(@Validated BoardPageRequestDTO boardPageRequestDTO){
+        log.info("pageController()---");
+        return ResponseEntity.ok(ApiResponse.success(boardService.page(boardPageRequestDTO)));
+    }
+
+    //게시글 전체 조회 (검색) --
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<ApiResponse> pageSearch(@PathVariable("keyword") String keyword,
+                                            @Validated BoardPageRequestDTO boardPageRequestDTO){
+        log.info("pageSearchController()---");
+        return ResponseEntity.ok(ApiResponse.success(boardService.pageSearch(keyword,boardPageRequestDTO)));
     }
 }
