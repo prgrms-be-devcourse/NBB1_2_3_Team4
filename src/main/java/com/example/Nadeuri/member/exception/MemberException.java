976cb6dbc997e63335eebc9a61df0a3753c6ed48
gt-park7356 <tlals0028@gmail.com>
2024-09-30 14:11:29 +0900

    refactor : DUPLICATED_MEMBER 추가

--- src/main/java/com/example/Nadeuri/member/exception/MemberException.java
+++ src/main/java/com/example/Nadeuri/member/exception/MemberException.java
@@ -1,11 +1,11 @@
 package com.example.Nadeuri.member.exception;
 
 public enum MemberException {
-    NOT_FOUND("존재하지 않는 회원입니다.", 404),
-    DUPLICATE("이미 존재하는 회원입니다.", 409),
+    NOT_FOUND("NOT_FOUND", 404),
+    DUPLICATE("DUPLICATED_MEMBER", 409),
     INVALID("INVALID", 400),
     BAD_CREDENTIALS("BAD_CREDENTIALS", 401),
-    NOT_MATCHED_USER("사용자가 일치하지 않습니다.", 400);
+    NOT_MATCHED_USER("NOT_MATCHED_USER", 400);
 
     private MemberTaskException memberTaskException;
 
