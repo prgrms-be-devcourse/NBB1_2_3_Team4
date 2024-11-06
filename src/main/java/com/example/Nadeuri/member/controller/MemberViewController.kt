ef1db5f84d9dfc43f71b5eb87bd55e4d63dc307f
gt-park7356 <tlals0028@gmail.com>
2024-11-06 09:19:07 +0900

    fix : 뷰 페이징 에러, 회원정보수정기능 수정 및 로그아웃 기능 구현

--- src/main/java/com/example/Nadeuri/member/controller/MemberViewController.kt
+++ src/main/java/com/example/Nadeuri/member/controller/MemberViewController.kt
@@ -3,16 +3,15 @@ package com.example.Nadeuri.member.controller
 import com.example.Nadeuri.member.MemberService
 import com.example.Nadeuri.member.dto.MemberDTO
 import com.example.Nadeuri.member.dto.request.MemberUpdateRequest
+import com.example.Nadeuri.member.security.util.CookieUtil
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequestMapping("/members")
@@ -23,17 +22,18 @@ class MemberViewController(private val memberService: MemberService) {
         return "login" // login.html 파일을 반환
     }
 
+    @GetMapping("/logout")
+    fun logout(request: HttpServletRequest, response: HttpServletResponse): String {
+        CookieUtil.deleteCookie(request, response, "accessToken")  // 액세스 토큰 쿠키 삭제
+        CookieUtil.deleteCookie(request, response, "refreshToken") // 리프레시 토큰 쿠키 삭제
+        return "redirect:/board/list"
+    }
+
     @GetMapping("/sign-up")
     fun signUp(): String {
         return "sign-up" // sign-up.html 파일을 반환
     }
 
-    // 로그아웃 메서드 추가
-    @GetMapping("/logout")
-    fun logout(request: HttpServletRequest, response: HttpServletResponse): String {
-        SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().authentication)
-        return "redirect:/members/login"
-    }
 
     // 회원정보 수정 페이지를 반환하는 메서드
     @GetMapping("/edit")
@@ -54,7 +54,6 @@ class MemberViewController(private val memberService: MemberService) {
         @ModelAttribute memberUpdateRequest: MemberUpdateRequest,
         @RequestPart(value = "image", required = false) profileImage: MultipartFile?,
         authentication: Authentication,
-        redirectAttributes: RedirectAttributes
     ): String {
         val dbUserId: String = authentication.name
         if (dbUserId != userId) {
@@ -63,7 +62,6 @@ class MemberViewController(private val memberService: MemberService) {
 
         memberService.updateMember(userId, memberUpdateRequest, profileImage)
 
-        redirectAttributes.addFlashAttribute("message", "회원정보가 성공적으로 수정되었습니다.")
-        return "redirect:/boards/list"
+        return "redirect:/board/list"
     }
 }
